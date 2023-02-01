b916c633f8ded30ec3cfaf13843c36ec49b529c6
Q-T5 <bikathimartin@outlook.com>
2023-02-01 20:25:21 +0300

    Add UI for the Chat Component

--- src/components/ChatComp.vue
+++ src/components/ChatComp.vue
@@ -15,26 +15,84 @@
 -->
 
 <script lang="js">
-import { inject } from 'vue'
+import { inject, ref } from 'vue'
 
 export default {
     name: "ChatComp",
-    setup() {
+    setup: function() {
         // receive injected user details
         const userDetails = inject("providedDetails");
+        const message = ref("");
+        const recipientId = ref("");
+        const receivedMessages = ref([]);
+
+        function sendMessage() {
+
+        }
+
+        function uploadMedia() {
+
+        }
 
         return {
-            userDetails
+            userDetails, sendMessage, uploadMedia, message, receivedMessages, recipientId
         }
+    },
+    mounted: function() {
+        alert("Refreshing the page destroys all your data");
     }
 }
 </script>
 
 <template>
-    <div>
-        <h1>Chat Component</h1>
-        <p>
-            UserName: {{ userDetails.userName }} & UserId: {{ userDetails.userId }}
-        </p>
+    <div class="w-full h-full p-10 flex border-[1px] space-x-2">
+        <div class="flex flex-col w-1/3 h-full rounded-md border-[1px]">
+            <div class="w-full flex flex-col items-center py-2 border-b-[1px]">
+                <h1 class="text-center">Registered As:</h1>
+                <div class="rounded-full bg-blue-400 w-20 h-20 flex justify-center items-center">
+                    <span class="text-3xl">HK</span>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col w-2/3 h-full rounded-md border-[1px]">
+            <div class="h-[10%] w-full flex justify-center items-center space-x-2 border-b-[1px]">
+                <label for="to-user-id">Send Message To: </label>
+                <input 
+                    type="text" 
+                    id="to-user-id"
+                    class="message-box w-[50%]"
+                    placeholder="Enter id of Recipient"
+                    v-model="recipientId" />
+            </div>
+            <div class="h-[80%] w-full overflow-y-scroll max-h-[90%]">
+
+            </div>
+            <div class="h-[10%] w-full flex justify-around items-center px-2">
+                <input 
+                    type="text" 
+                    id="message-box" 
+                    class="message-box"
+                    placeholder="Enter Message..."
+                    v-model="message" />
+                <div class="w-[20%] flex justify-center space-x-2">
+                    <button 
+                        class="action-button"
+                        @click="sendMessage">
+                        <img 
+                            src="src/assets/send_message.svg" 
+                            alt="send-message"
+                            class="w-6" />
+                    </button>
+                    <button 
+                        class="action-button"
+                        @click="uploadMedia">
+                        <img 
+                            src="src/assets/upload_media.svg" 
+                            alt="send-message"
+                            class="w-6" />
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
