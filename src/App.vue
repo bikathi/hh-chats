009baa1a09dd64734034290323ca7a2140f48409
Q-T5 <bikathimartin@outlook.com>
2023-02-01 06:11:45 +0300

    Mount components into VueComponent

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,41 @@
 <script lang="js">
+import RegisterComp from './components/RegisterComp.vue'
+import ChatComp from './components/ChatComp.vue'
+import { ref, reactive, provide } from 'vue'
+
 export default {
   name: "App",
+  components: {
+    RegisterComp, ChatComp
+  },
   setup() {
+    const comp = ref("register-comp");
+    const details = reactive({
+      userName: "",
+      userId: ""
+    });
 
+    // provide logged in user-details
+    provide('providedDetails', details);
+
+    function startChat(userDetails) {
+      details.userName = userDetails.userName;
+      details.userId = userDetails.userId;
+
+      comp.value = "chat-comp";
+    }
+
+    return {
+      comp, startChat, details
+    }
   }
 }
 </script>
 
 <template>
-  <div>
-    <h1 class="text-3xl">Hello World</h1>
+  <div class="w-screen h-screen">
+    <component 
+      :is="comp"
+      @start-chat="startChat" />
   </div>
 </template>
